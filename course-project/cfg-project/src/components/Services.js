d65afbaebe7d1e3fce970676c6d9615842d54200
Anesu <106879570+Anesu-R@users.noreply.github.com>
2023-03-31 23:41:17 +0100

    Update Services.js

--- course-project/cfg-project/src/components/Services.js
+++ course-project/cfg-project/src/components/Services.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import SubscribeSection from './SubscribeSection';
+import ServicesHeader from './ServicesHeader';
+import ServicesContent from './ServicesContent';
 
 class Services extends Component {
     render() {
         return (
-            <div>
-                <h2>Services Page</h2>
+            <div className="servicesPage">
+                <ServicesHeader/>
+                    <div className="service-section service-section-three section-padding">
+                        <ServicesContent/>
+                    </div>
+                <SubscribeSection/>
             </div>
         );
     }
