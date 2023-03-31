e0cfc75e1faae6a779cbd3b6f0ec4f90afc8956f
Anesu <106879570+Anesu-R@users.noreply.github.com>
2023-03-31 23:39:22 +0100

    Update Pricing.js

--- course-project/cfg-project/src/components/Pricing.js
+++ course-project/cfg-project/src/components/Pricing.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import SubscribeSection from './SubscribeSection';
+import PricingHeader from './PricingHeader';
+import PricingContent from './PricingContent';
 
 class Pricing extends Component {
     render() {
         return (
-            <div>
-                <h2>Pricing Page</h2>
+            <div className="pricingPage">
+                <PricingHeader/>
+                    <div className="pricing-section section-padding">
+                        <PricingContent/>
+                    </div>
+                <SubscribeSection/>
             </div>
         );
     }
