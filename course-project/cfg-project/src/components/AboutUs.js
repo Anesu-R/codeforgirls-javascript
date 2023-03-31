fbce31842fa9ea4cf36deda91799457b6139834a
Anesu <106879570+Anesu-R@users.noreply.github.com>
2023-03-31 23:37:11 +0100

    Update and rename About.js to AboutUs.js

---
+++ course-project/cfg-project/src/components/AboutUs.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import SubscribeSection from './SubscribeSection';
+import AboutUsHeader from './AboutUsHeader';
+import AboutUsContent from './AboutUsContent';
+
+class AboutUs extends Component {
+    render() {
+        return (
+            <div className="aboutPage">
+                <AboutUsHeader/>
+                    <div className="about-section-two section-padding">
+                        <AboutUsContent/>
+                    </div>
+                <SubscribeSection/>
+            </div>
+        );
+    }
+}
+
+export default AboutUs;
