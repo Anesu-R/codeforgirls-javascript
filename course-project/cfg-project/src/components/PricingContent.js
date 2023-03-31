2374c011c7fbcc6ae0fcf049a661c37809f3e80e
Anesu <106879570+Anesu-R@users.noreply.github.com>
2023-03-31 23:40:11 +0100

    Create PricingContent.js

---
+++ course-project/cfg-project/src/components/PricingContent.js
@@ -0,0 +1,312 @@
+import React, { Component } from 'react';
+
+class PricingContent extends Component {
+    render() {
+        return (
+            <div class="container">
+            <div class="section-title text-center">
+                <h2>Our <span class="text-color">Pricing</span></h2>
+            </div> 
+
+            <div class="pricing-wrapper">
+                <div class="row">
+                    <div class="col-md-4">
+                        <ul class="nav nav-tabs" role="tablist">
+                            <li role="presentation" class="active"><a href="#tab-one" role="tab" data-toggle="tab">
+                                <img src="assets/images/shop/i1.png" alt=""/>
+
+                                <div class="tab-content">
+                                    <h5>Garden Maintenance</h5>
+                                    <p>From <span class="text-color">$55</span> for a team of 2 professionals</p>
+                                </div> 
+                            </a></li>
+
+                            <li role="presentation" ><a href="#tab-two" role="tab" data-toggle="tab">
+                                <img src="assets/images/shop/i2.png" alt=""/>
+
+                                <div class="tab-content">
+                                    <h5>Landscaping & Design</h5>
+                                    <p>From <span class="text-color">$55</span> for a team of 2 professionals</p>
+                                </div> 
+                            </a></li>
+
+                            <li role="presentation"><a href="#tab-three" role="tab" data-toggle="tab">
+                                <img src="assets/images/shop/i3.png" alt=""/>
+
+                                <div class="tab-content">
+                                    <h5>Lawn Care</h5>
+                                    <p>From <span class="text-color">$55</span> for a team of 2 professionals</p>
+                                </div> 
+                            </a></li>
+
+                            <li role="presentation"><a href="#tab-four" role="tab" data-toggle="tab">
+                                <img src="assets/images/shop/i4.png" alt=""/>
+
+                                <div class="tab-content">
+                                    <h5>Tree Surgery</h5>
+                                    <p>From <span class="text-color">$55</span> for a team of 2 professionals</p>
+                                </div> 
+                            </a></li>
+
+                            <li role="presentation"><a href="#tab-five" role="tab" data-toggle="tab">
+                                <img src="assets/images/shop/i5.png" alt=""/>
+
+                                <div class="tab-content">
+                                    <h5>Outdoor Repairs</h5>
+                                    <p>From <span class="text-color">$55</span> for a team of 2 professionals</p>
+                                </div> 
+                            </a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-8">
+                        <div class="tab-content text-center">
+                            <div role="tabpanel" class="tab-pane fade in active" id="tab-one">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="priceing-table">
+                                            <h4>Basic</h4>
+                                            <h5 class="amount">$55</h5>
+                                            <p>Basic garden maintenance service includes the following procedures:</p>
+
+                                            <ul class="option">
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Lawn mowing</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Weeding</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Hedge trimming</li>
+
+                                                <li><i class="fa fa-times" aria-hidden="true"></i>Leaf clearance</li>
+
+                                                <li><i class="fa fa-times" aria-hidden="true"></i>vy and removal</li>
+                                            </ul> 
+
+                                            <a href="#" class="btn btn-primary">Select</a>
+                                        </div> 
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <div class="priceing-table">
+                                            <h4>Premium</h4>
+                                            <h5 class="amount">$95</h5>
+                                            <p>Basic garden maintenance service includes the following procedures:</p>
+
+                                            <ul class="option">
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Lawn mowing</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Weeding</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Hedge trimming</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Leaf clearance</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>vy and removal</li>
+                                            </ul> 
+
+                                            <a href="#" class="btn btn-primary">Select</a>
+                                        </div> 
+                                    </div>
+                                </div>
+                            </div> 
+
+                            <div role="tabpanel" class="tab-pane tab-story fade " id="tab-two">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="priceing-table">
+                                            <h4>Premium</h4>
+                                            <h5 class="amount">$65</h5>
+                                            <p>Basic garden maintenance service includes the following procedures:</p>
+
+                                            <ul class="option">
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Lawn mowing</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Weeding</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Hedge trimming</li>
+
+                                                <li><i class="fa fa-times" aria-hidden="true"></i>Leaf clearance</li>
+
+                                                <li><i class="fa fa-times" aria-hidden="true"></i>vy and removal</li>
+                                            </ul> 
+
+                                            <a href="#" class="btn btn-primary">Select</a>
+                                        </div> 
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <div class="priceing-table">
+                                            <h4>Basic</h4>
+                                            <h5 class="amount">$55</h5>
+                                            <p>Basic garden maintenance service includes the following procedures:</p>
+
+                                            <ul class="option">
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Lawn mowing</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Weeding</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Hedge trimming</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Leaf clearance</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>vy and removal</li>
+                                            </ul> 
+
+                                            <a href="#" class="btn btn-primary">Select</a>
+                                        </div> 
+                                    </div>
+                                </div>
+                            </div> 
+
+                            <div role="tabpanel" class="tab-pane fade" id="tab-three">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="priceing-table">
+                                            <h4>Basic</h4>
+                                            <h5 class="amount">$55</h5>
+                                            <p>Basic garden maintenance service includes the following procedures:</p>
+
+                                            <ul class="option">
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Lawn mowing</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Weeding</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Hedge trimming</li>
+
+                                                <li><i class="fa fa-times" aria-hidden="true"></i>Leaf clearance</li>
+
+                                                <li><i class="fa fa-times" aria-hidden="true"></i>vy and removal</li>
+                                            </ul> 
+
+                                            <a href="#" class="btn btn-primary">Select</a>
+                                        </div> 
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <div class="priceing-table">
+                                            <h4>Premium</h4>
+                                            <h5 class="amount">$95</h5>
+                                            <p>Basic garden maintenance service includes the following procedures:</p>
+
+                                            <ul class="option">
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Lawn mowing</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Weeding</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Hedge trimming</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Leaf clearance</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>vy and removal</li>
+                                            </ul> 
+
+                                            <a href="#" class="btn btn-primary">Select</a>
+                                        </div> 
+                                    </div>
+                                </div>
+                            </div> 
+
+                            <div role="tabpanel" class="tab-pane fade" id="tab-four">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="priceing-table">
+                                            <h4>Basic</h4>
+                                            <h5 class="amount">$55</h5>
+                                            <p>Basic garden maintenance service includes the following procedures:</p>
+
+                                            <ul class="option">
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Lawn mowing</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Weeding</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Hedge trimming</li>
+
+                                                <li><i class="fa fa-times" aria-hidden="true"></i>Leaf clearance</li>
+
+                                                <li><i class="fa fa-times" aria-hidden="true"></i>vy and removal</li>
+                                            </ul> 
+
+                                            <a href="#" class="btn btn-primary">Select</a>
+                                        </div> 
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <div class="priceing-table">
+                                            <h4>Premium</h4>
+                                            <h5 class="amount">$95</h5>
+                                            <p>Basic garden maintenance service includes the following procedures:</p>
+
+                                            <ul class="option">
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Lawn mowing</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Weeding</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Hedge trimming</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Leaf clearance</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>vy and removal</li>
+                                            </ul> 
+
+                                            <a href="#" class="btn btn-primary">Select</a>
+                                        </div> 
+                                    </div>
+                                </div>
+                            </div> 
+
+                            <div role="tabpanel" class="tab-pane fade" id="tab-five">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="priceing-table">
+                                            <h4>Basic</h4>
+                                            <h5 class="amount">$55</h5>
+                                            <p>Basic garden maintenance service includes the following procedures:</p>
+
+                                            <ul class="option">
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Lawn mowing</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Weeding</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Hedge trimming</li>
+
+                                                <li><i class="fa fa-times" aria-hidden="true"></i>Leaf clearance</li>
+
+                                                <li><i class="fa fa-times" aria-hidden="true"></i>vy and removal</li>
+                                            </ul> 
+
+                                            <a href="#" class="btn btn-primary">Select</a>
+                                        </div> 
+                                    </div>
+
+                                    <div class="col-sm-6">
+                                        <div class="priceing-table">
+                                            <h4>Premium</h4>
+                                            <h5 class="amount">$95</h5>
+                                            <p>Basic garden maintenance service includes the following procedures:</p>
+
+                                            <ul class="option">
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Lawn mowing</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Weeding</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Hedge trimming</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>Leaf clearance</li>
+
+                                                <li><i class="fa fa-check" aria-hidden="true"></i>vy and removal</li>
+                                            </ul> 
+
+                                            <a href="#" class="btn btn-primary">Select</a>
+                                        </div> 
+                                    </div>
+                                </div>
+                            </div> 
+                        </div> 
+                    </div>
+                </div>
+            </div> 
+        </div>        
+        );
+    }
+}
+
+export default PricingContent;
